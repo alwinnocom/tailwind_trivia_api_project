55877a091b3df8b06ca80e2b140874033b59d790
[Alwin] <[alwinnocom@gmail.com]>
2020-10-10 11:14:05 -0700

    1st revision: Add https module + response.on. JSON parse successful.

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 //jshint esversion:6
 
+const https = require("https");
+const url = "https://opentdb.com/api.php?amount=10";    
+
 const express = require('express'); 
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(3000, function() {
+app.listen(3000, () => {
     console.log("Server started on port 3000.");
 });
+
+
+// HTTPS Module to parse JSON.
+
+https.get(url, (response) => {
+    
+    response.on('data', (data) => {
+        newData = JSON.parse(data);
+        console.log(newData);
+    });
+
+    app.post("/", (req, res) => {
+        res.render(newData);
+    })
+
+})
+
+
+
+
+
+
+
