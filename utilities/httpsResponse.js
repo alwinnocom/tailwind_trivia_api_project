d8efb04691cbf4738b2a85206a30a84f1e89e77a
[Alwin] <[alwinnocom@gmail.com]>
2020-10-24 13:26:35 -0700

    Oct 24 1st commit. Mongoose for loop successful.

--- utilities/httpsResponse.js
+++ utilities/httpsResponse.js
@@ -5,75 +5,31 @@ const httpsResponse = () => {
     https.get(url, (response) => {
         
         response.on('data', (data) => {
-            newData = JSON.parse(data);
 
-            // console.log(newData);
-            // console.log(newData.results);
-            // console.log(newData.results[0]);
-            // let questionData = ""
-            // for (i = 0; i++; i < numberOfQuestions) {
-            //     questionData = newData.results[i];
-            //     console.log(questionData);
+            const newData = JSON.parse(data);
+            
+            const questionData = newData.results;
 
-                let questionData = newData.results[0];
-                
-                // [questionCategory, questionType, questionDifficulty, question, correctAnswer, incorrectAnswers] = [questionData.category, questionData.type, questionData.difficulty, questionData.question, questionData.correct_answer, questionData.incorrect_answers];
-                
-                // const questionOne = new Question({
-                //     questionCategory: questionCategory,
-                //     questionType: questionType,
-                //     questionDifficulty: questionDifficulty, 
-                //     question: question,
-                //     correctAnswer: correctAnswer,
-                //     incorrectAnswers: incorrectAnswers
-                // })
-                
-                const questionOne = new Question({
-                    questionCategory: questionData.category,
-                    questionType: questionData.type,
-                    questionDifficulty: questionData.difficulty, 
-                    question: questionData.question,
-                    correctAnswer: questionData.correct_answer,
-                    incorrectAnswers: questionData.incorrect_answers
-                })
+                for (i = 0; i < questionData.length; i++) {
 
+                    let questionOne = new Question({
+                        questionCategory: questionData[i].category,
+                        questionType: questionData[i].type,
+                        questionDifficulty: questionData[i].difficulty, 
+                        question: questionData[i].question,
+                        correctAnswer: questionData[i].correct_answer,
+                        incorrectAnswers: questionData[i].incorrect_answers
+                    })
 
-                questionOne.save();
+                    console.log(`Question One is currently ${questionOne}`);
+                    
+                    questionOne.save();
 
-                // console.log(`In the HTTPS Response, Question Category = ${questionCategory}`);
-                
-                // console.log(`Question Category finishes as ${questionCategory}, Question Type = ${questionType}, Question Difficulty = ${questionDifficulty}, Question = ${question}, Correct Answer = ${correctAnswer}, Incorrect Answers = ${incorrectAnswers}`);
-                
-                // if (questionCategory && questionType && questionDifficulty && question && correctAnswer && incorrectAnswers) {
-                //     app.route("/results")
-                //         .get((req, res) => {
-                //             res.render("results", {
-                //                             questionCategory: questionCategory,
-                //                             questionType: questionType,
-                //                             questionDifficulty: questionDifficulty,
-                //                             question: question,
-                //                             correctAnswer: correctAnswer,
-                //                             incorrectAnswers: incorrectAnswers
-                //                         });
+                }
 
-                //             //  console.log(`Question Data = ${questionData}`)
-                //             console.log(`Question Category finishes as ${questionCategory}, Question Type = ${questionType}, Question Difficulty = ${questionDifficulty}, Question = ${question}, Correct Answer = ${correctAnswer}, Incorrect Answers = ${incorrectAnswers}`);
-                //         })
-                // }
-
-                // console.log("Https response received.");
-                // console.log(`Question Category = ${questionCategory}, Question Type = ${questionType}, Question Difficulty = ${questionDifficulty}, Question = ${question}, Correct Answer = ${correctAnswer}, Incorrect Answers = ${incorrectAnswers}`)
             });
 
     })
 }
 
 module.exports = httpsResponse;
-
-// https://stackoverflow.com/questions/32558514/javascript-es6-export-const-vs-export-let
-// module.exports.questionCategory = questionCategory;
-// module.exports.questionType = questionType;
-// module.exports.questionDifficulty = questionDifficulty;
-// module.exports.question = question;
-// module.exports.correctAnswer = correctAnswer;
-// module.exports.incorrectAnswers = incorrectAnswers;
